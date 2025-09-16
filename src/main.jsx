2a0245e7ad7d063cd4d700e5910745078ec4731a
Harrison | The Cybermaniac <thetruebeastist@gmail.com>
2025-09-16 15:40:55 +0100

    First Lesson

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ThemeProvider } from "./context/ThemeContext.js";
+import { AppWrapper } from "./components/common/PageMeta.tsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider>
+      <AppWrapper>
+        <App />
+      </AppWrapper>
+    </ThemeProvider>
+  </StrictMode>
+);
---
+++ src/pages/auth/SignIn.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignIn = () => {
+  return <div>This is the Sign In page</div>;
+};
+
+export default SignIn;
