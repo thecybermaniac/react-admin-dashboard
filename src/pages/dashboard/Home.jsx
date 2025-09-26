238b898458fa18346f52a4d683cb041c29a41300
Winezky Umpire <ezewinner5265@gmail.com>
2025-09-26 15:41:42 -0700

    react-admin-dashboard update

--- src/pages/dashboard/Home.jsx
+++ src/pages/dashboard/Home.jsx
@@ -3,10 +3,46 @@ import { account } from "../../lib/appwrite";
 import { useState } from "react";
 import { useEffect } from "react";
 import Loader from "../../components/Loader";
+import MonthlySalesChart from "../../components/ecommerce/MonthlySalesChart";
+import EcommerceMetrics from "../../components/ecommerce/EcommerceMetrics";
+import PageMeta from "../../components/common/PageMeta";
+import MonthlyTarget from "../../components/ecommerce/MonthlyTarget";
+import StatisticsChart from "../../components/ecommerce/StatisticsChart";
+import DemographicCard from "../../components/ecommerce/DemographicCard";
+import RecentOrders from "../../components/ecommerce/RecentOrders";
 
 const Home = () => {
 
-  return <div>This is the dashboard page</div>;
+  return (
+    <>
+      <PageMeta
+        title="React.js Ecommerce Dashboard | TailAdmin - React.js Admin Dashboard Template"
+        description="This is React.js Ecommerce Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
+      />
+      <div className="grid grid-cols-12 gap-4 md:gap-6">
+        <div className="col-span-12 space-y-6 xl:col-span-7">
+          <EcommerceMetrics />
+          <MonthlySalesChart />
+        </div>
+
+        <div className="col-span-12 xl:col-span-5">
+          <MonthlyTarget />
+        </div>
+
+        <div className="col-span-12">
+          <StatisticsChart />
+        </div>
+
+        <div className="col-span-12 xl:col-span-5">
+          <DemographicCard />
+        </div>
+
+        <div className="col-span-12 xl:col-span-7">
+          <RecentOrders />
+        </div>
+      </div>
+    </>
+  )
 };
 
 export default Home;
